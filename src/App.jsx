f788457373cdd0f7a219e05d91fcf48ddfc226b4
Diallo_Codeur <mouhammadsitadiallo@gmail.com>
2026-02-09 13:56:03 +0100

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 class="text-8xl font-bold line-over">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
